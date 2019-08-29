5dd0c2403c91b4158a89945013b342dc4701bc58
elp4nsho <l.m_d.m@hotmail.com>
2019-08-29 04:38:54 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild("mapCanvas",{static:false}) mapCanvas:ElementRef;
+  map: GoogleMap;
 
   constructor() {}
 
+  loadMap() {
+    let element = this.mapCanvas.nativeElement
+    this.map = GoogleMaps.create(element, {
+      camera: {
+        target: {
+          lat: 43.0741704,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    });
+
+  }
 }
